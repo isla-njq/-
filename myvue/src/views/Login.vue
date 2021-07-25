f1b1365587d651ca163fb6e8a1a9b71124ec0428
isla-njq <2234080905@qq.com>
2021-07-25 18:29:55 +0800

    push

--- myvue/src/views/Login.vue
+++ myvue/src/views/Login.vue
@@ -1,15 +1,17 @@
 <template>
-    <div>
-        <div>账户登录</div>
-        <form>
-            用户名:<input type="text" name="username" ref="userid"/>
-            密码:<input type="password" name="userpassword" ref="userpassword"/>
-        </form>
-        <form>
-            <input type="radio" name="usertype" value="Administrator" v-model="looplist"/>管理员
-            <input type="radio" name="usertype" value="merchant" v-model="looplist"/>商家
-        </form>
-        <button @click="userlogin">登录</button>
+    <div class="main">
+        <div><h1>账户登录</h1></div>
+        <div class="login_main">
+            <form>
+                用户名：<input type="text" name="username" ref="userid"/></br>
+                密　码：<input type="password" name="userpassword" ref="userpassword"/>
+            </form>
+            <form>
+                <input type="radio" name="usertype" value="Administrator" v-model="looplist"/>管理员
+                <input type="radio" name="usertype" value="merchant" v-model="looplist"/>商家
+            </form>
+            <button @click="userlogin">登录</button>
+        </div>
     </div>
 </template>
 
@@ -25,15 +27,18 @@ export default {
             looplist:""
         }
     },
-    methods: {
+    methods:{
             userlogin(){
-
+                if(this.$refs.userid.value.length == 0){
+                    alert("请输入账号")
+                }else if(this.$refs.userpassword.value.length==0||this.$refs.userid.value.length == 0){
+                    alert("账号或密码错误")
+                };
                 console.log(this.looplist,this.$refs.userid.value.length,this.$refs.userpassword.value.length);
                 let data = {
                     username: this.$refs.userid.value,
                     password: this.$refs.userpassword.value,
-                };
-
+                };                
                 axios({
                     headers: {
                         'Content-Type': 'application/x-www-form-urlencoded'
@@ -52,3 +57,13 @@ export default {
         }
 }
 </script>
+
+<style scoped>
+div{
+    background-color: rgb(217, 232, 251);
+}
+.login_main{
+    padding: 30px;
+    color: rgb(30, 100, 152);
+}
+</style>
