036af61de300235aa348d0c1c70fa792be71c5a4
isla-njq <2234080905@qq.com>
2021-08-09 15:43:40 +0800

    njq 8/9

--- myvue/src/views/Shops/Authority_management/Mone_Management.vue
+++ myvue/src/views/Shops/Authority_management/Mone_Management.vue
@@ -1,18 +1,63 @@
 <!--资金管理-->
 <template>
-    <div class="main">
+    <div class="main" >
         <main>
             <section class="section2">
-                <el-breadcrumb separator-class="el-icon-arrow-right" class="el-breadcrumb-size">
-                    <el-breadcrumb-item :to="{ path: '/' }">权限管理</el-breadcrumb-item>
+                <el-breadcrumb separator-class="el-icon-arrow-right">
+                    <el-breadcrumb-item>权限管理</el-breadcrumb-item>
                     <el-breadcrumb-item>资金管理</el-breadcrumb-item>
                 </el-breadcrumb>
-                <div>
-                    主要内容
+                <div :style="'height:'+fullHeight+'px;'" class="div">
+                    <div class="Running_water">
+                        <el-table
+                            :data="Money_statistics"
+                            style="width: 100%">
+                        <el-table-column
+                            prop="order_number"
+                            label="订单编号"
+                            style="color: green">
+                        </el-table-column>
+                        <el-table-column
+                            prop="trading_time"
+                            label="支付时间"
+                            >
+                        </el-table-column>
+                        <el-table-column
+                            prop="trading_type"
+                            label="支付方式"
+                            >
+                        </el-table-column>
+                        <el-table-column
+                            prop="order_money"
+                            label="订单金额"
+                            >
+                        </el-table-column>
+                        <el-table-column
+                            prop="Coupon_money"
+                            label="优惠券金额"
+                            >
+                        </el-table-column>
+                        <el-table-column
+                            prop="pay_money"
+                            label="支付金额"
+                            >
+                        </el-table-column>
+                        <el-table-column
+                            prop="store"
+                            label="店铺名称"
+                            >
+                        </el-table-column>
+                        <el-table-column
+                            prop="members"
+                            label="会员(手机号)"
+                            >
+                        </el-table-column>
+                        </el-table>
+                    </div>
                 </div>
+                <Foot class="foot"></Foot>
             </section>
         </main>
-        <Foot class="foot"></Foot>
     </div>
 </template>
 
@@ -21,59 +66,104 @@ import Foot from '../../../components/firm_background_foot.vue'
 export default {
     data(){
         return{
-            shopslist:[
+            Money_statistics: [
                 {
-                    id:1,
-                    name:'小熊杂货店',
-                    commodity:'可乐',
-                    price:24000,
-                    sales:550,
-                    inventory:286,
+                    
+                    order_number: 11323213213,
+                    trading_time:'2020-1-1 16:00:00',
+                    trading_type:'支付宝',
+                    order_money:24000,
+                    Coupon_money:0,
+                    pay_money: 24000,
+                    store: '小熊杂货店',
+                    members:'1112321321'
                 },{
-                    id:2,
-                    name:'小熊杂货店',
-                    commodity:'可乐',
-                    price:24000,
-                    sales:550,
-                    inventory:286,
+                    
+                    order_number: 11323213213,
+                    trading_time:'2020-1-1 16:00:00',
+                    trading_type:'支付宝',
+                    order_money:24000,
+                    Coupon_money:10000,
+                    pay_money: 14000,
+                    store:'小熊杂货店',
+                    members:'1112321321'
                 }
-            ]
+            ],
+            fullHeight: document.documentElement.clientHeight - 195
         }
     },
     components:{
         Foot
     },
-
+    watch: {
+			fullHeight (val) {//监控浏览器高度变化
+				if(!this.timer) {
+					this.fullHeight = val
+					this.timer = true
+					let that = this
+					setTimeout(function (){
+						that.timer = false
+					},400)
+				}
+				
+			}
+		},
+	mounted () {
+		this.get_bodyHeight()
+	},
+	methods :{
+		get_bodyHeight () {//动态获取浏览器高度
+			const that = this
+			window.onresize = () => {
+				return (() => {
+					window.fullHeight = document.documentElement.clientHeight
+					that.fullHeight = window.fullHeight - 195
+				})()
+			}
+		}
+	}
 }
 </script>
 
 <style scoped>
-main{
+.main{
     overflow: hidden;
     position: relative;
+    background-color: rgb(244, 246, 250);
+    max-height: 1180px;
 }
 
-.el-breadcrumb-size{
+.el-breadcrumb{
+    background-color: white;
     font-size: 26px;
     line-height: 45px;
+    padding-left: 30px;
 }
+
+.div{
+    width: 100%;
+}
+
+.Running_water{
+    overflow: hidden;
+    margin-top: 50px;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+}
+
 .section2{
-    width: 90%;
+    float: right;
+    width: 97%;
     text-align: center;
     overflow: hidden;
 }
 .section3{
+    margin: 3px;
     position: absolute;
     right: 100px;
-    top: 155px;
-}
-table
-{
-	width:100%;
-}
-th
-{
-	height:50px;
+    top: 0px;
+    
 }
 .foot{
     position:fixed;
--- myvue/src/views/Shops/invoice_management/new_invoice.vue
+++ myvue/src/views/Shops/invoice_management/new_invoice.vue
@@ -1,18 +1,20 @@
-<!--全部申请/开票-->
+<!--添加商品-->
 <template>
-    <div class="main">
+    <div class="main" >
         <main>
             <section class="section2">
-                <el-breadcrumb separator-class="el-icon-arrow-right" class="el-breadcrumb-size">
-                    <el-breadcrumb-item :to="{ path: '/' }">全部申请</el-breadcrumb-item>
-                    <el-breadcrumb-item>开票</el-breadcrumb-item>
+                <el-breadcrumb separator-class="el-icon-arrow-right">
+                    <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+                    <el-breadcrumb-item>已发布</el-breadcrumb-item>
+                    <el-breadcrumb-item>添加商品</el-breadcrumb-item>
                 </el-breadcrumb>
-                <div>
-                    主要界面
+                <section class="section3"><el-button type="primary">添加商品</el-button></section>
+                <div :style="'height:'+fullHeight+'px;'">
+                    
                 </div>
             </section>
         </main>
-        <section class="section3"><el-button type="primary">添加商品</el-button></section>
+        
         <Foot class="foot"></Foot>
     </div>
 </template>
@@ -22,59 +24,69 @@ import Foot from '../../../components/firm_background_foot.vue'
 export default {
     data(){
         return{
-            shopslist:[
-                {
-                    id:1,
-                    control_No:181515156,
-                    price:10,
-                    time1:"2012-02-05",
-                    time2:'2012-02-05',
-                    time3:"2012-02-05",
-                    comment:"无",
-                },{
-                    control_No:181515157,
-                    price:1000,
-                    time1:"2012-02-05",
-                    time2:'2012-02-05',
-                    time3:"2012-02-05",
-                    comment:"无",
-                }
-            ]
+            
+            fullHeight: document.documentElement.clientHeight - 195
         }
     },
     components:{
         Foot
     },
-
+    watch: {
+			fullHeight (val) {//监控浏览器高度变化
+				if(!this.timer) {
+					this.fullHeight = val
+					this.timer = true
+					let that = this
+					setTimeout(function (){
+						that.timer = false
+					},400)
+				}
+				
+			}
+		},
+	mounted () {
+		this.get_bodyHeight()
+	},
+	methods :{
+		get_bodyHeight () {//动态获取浏览器高度
+			const that = this
+			window.onresize = () => {
+				return (() => {
+					window.fullHeight = document.documentElement.clientHeight
+					that.fullHeight = window.fullHeight - 195
+				})()
+			}
+		}
+	}
 }
 </script>
 
 <style scoped>
-main{
+.main{
     overflow: hidden;
+    position: relative;
+    background-color: rgb(244, 246, 250);
+    max-height: 1180px;
 }
 
-.el-breadcrumb-size{
+.el-breadcrumb{
+    background-color: white;
     font-size: 26px;
     line-height: 45px;
+    padding-left: 30px;
 }
 .section2{
-    width: 90%;
+    float: right;
+    width: 97%;
     text-align: center;
     overflow: hidden;
 }
 .section3{
+    margin: 3px;
     position: absolute;
     right: 100px;
-    top: 155px;
-}
-table
-{
-	width:100%;
-}
-th
-{
-	height:50px;
+    top: 0px;
+    
 }
 .foot{
     position:fixed;
